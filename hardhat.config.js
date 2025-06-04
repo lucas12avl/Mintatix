fdb698b932ed671df96dc1ff2874d27563f0f57f
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2025-06-04 13:54:40 +0200

    minor changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,10 @@ require("@nomicfoundation/hardhat-toolbox");
 
 const { vars } = require("hardhat/config");
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY") || "";
+;
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY") || "";
+;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
