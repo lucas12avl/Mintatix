bb56e84170ff5b1517e0b6235deba55f97cd4a6a
lucas12avl <73055673+lucas12avl@users.noreply.github.com>
2025-06-04 14:06:06 +0200

    minor changes v2

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,21 +2,27 @@ require("@nomicfoundation/hardhat-toolbox");
 
 const { vars } = require("hardhat/config");
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY") || "";
-;
-const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY") || "";
-;
+let INFURA_API_KEY = "";
+let SEPOLIA_PRIVATE_KEY = "";
+
+try {
+  INFURA_API_KEY = vars.get("INFURA_API_KEY");
+  SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+} catch (error) {
+  console.warn("Loca mode: skip Sepolia network configuration");
+}
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   defaultNetwork: "localhost",
   networks: {
-    ...(SEPOLIA_PRIVATE_KEY && INFURA_API_KEY)?{
-      sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
-      },
-    }:{}  
+  ...(INFURA_API_KEY && SEPOLIA_PRIVATE_KEY ? {
+          sepolia: {
+            url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+          },
+        }
+    :{}),
   },
 };
